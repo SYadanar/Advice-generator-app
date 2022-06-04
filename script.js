cded9f086876ce4f08ab670961baea3b902aaaa3
SYadanar <suyadanar2001@gmail.com>
2022-06-04 13:22:06 +0630

    Add loading spinner

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 const adviceID = document.getElementById("advice-id");
 const adviceText = document.getElementById("advice-text");
 const dice = document.getElementById("dice");
+const main = document.getElementById("main-content");
+const loadingSpinner = document.getElementById("loading-spinner");
 
 const getAdvice = () => fetch('https://api.adviceslip.com/advice')
 	.then(res => {
 		return res.json();
 	})
 	.then(data => {
-		console.log(data);
+		// console.log(data);
 		adviceID.textContent = data.slip.id;
 		adviceText.textContent = data.slip.advice;
+
+		main.classList.remove("d-none");
+		loadingSpinner.classList.add("d-none");
 	})
 	.catch(error => console.log("ERROR"))
 
 
 dice.addEventListener("click", () => {
+	main.classList.add("d-none");
+	loadingSpinner.classList.remove("d-none");
 	getAdvice();
 });
